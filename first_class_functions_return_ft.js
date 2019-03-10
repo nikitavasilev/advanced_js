fa8e2ddc4903a991e9c5c614725a996b04258cae
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-10 15:02:59 +0100

    functions returning functions

---
+++ first_class_functions_return_ft.js
@@ -0,0 +1,28 @@
+// First-Class Funtions: Functions returning functions
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what UX designer is?');
+    };
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What subject do you teach, ' + name + '?');
+    };
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    };
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
