9f60b5b78d5d90cbdf735216790c552d7eaf7306
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-11 22:04:28 +0100

    all working

--- coding_challenge/main.js
+++ coding_challenge/main.js
@@ -1,44 +1,48 @@
-// 1
-function Question(question, answers, correct) {
-  this.question = question;
-  this.answers = answers;
-  this.correct = correct;
-};
+(function () {
+  function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+  };
 
-// 2
-const firstQuestion = new Question(
-  'Is JavaScript the coolest programming language in the world?',
-  ['Yes', 'No'],
-  0
-);
+  const firstQuestion = new Question(
+    'Is JavaScript the coolest programming language in the world?',
+    ['Yes', 'No'],
+    0
+  );
 
-const secondQuestion = new Question(
-  'What is the name of this course\'s teacher?',
-  ['John', 'Mike', 'Jonas'],
-  2
-);
+  const secondQuestion = new Question(
+    'What is the name of this course\'s teacher?',
+    ['John', 'Mike', 'Jonas'],
+    2
+  );
 
-const thirdQuestion = new Question(
-  'What does best describe coding?',
-  ['Boring', 'Hard', 'Fun', 'Tedious'],
-  2
-);
+  const thirdQuestion = new Question(
+    'What does best describe coding?',
+    ['Boring', 'Hard', 'Fun', 'Tedious'],
+    2
+  );
 
-// 3
-const questions = [firstQuestion, secondQuestion, thirdQuestion];
+  const questions = [firstQuestion, secondQuestion, thirdQuestion];
+  const num = Math.floor(Math.random() * questions.length);
 
-// 4
-function randomSelect(min, max) {
-  const num = Math.floor(Math.random() * (max - min)) + min;
-  console.log(questions[num].question);
+  function displayQuestion() {
+    console.log(questions[num].question);
 
-  for (let i = 0; i < questions[num].answers.length; i++) {
-    console.log(i + ': ' + questions[num].answers[i]);
+    for (let i = 0; i < questions[num].answers.length; i++) {
+      console.log(i + ': ' + questions[num].answers[i]);
+    }
   }
-}
 
-// 5
-let answer = prompt('Please select the correct answer.');
+  function checkAnswer(clientAnswer) {
+    if (clientAnswer === questions[num].correct) {
+      console.log('Correct answer!');
+    } else {
+      console.log('Wrong answer. Try again :)')
+    }
+  }
 
-// Logs
-randomSelect(0, questions.length);
+  displayQuestion();
+  let answer = parseInt(prompt('Please select the correct answer.'));
+  checkAnswer(answer);
+})();
